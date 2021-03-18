52b72caaf2c00c8ccd7e09d4ec0920427bc67a79
Josias Alvarado <josiasjag@gmail.com>
2021-03-18 12:10:01 -0600

    Add docstrings for all methods and classes

--- pgn2fixture/pgn2fixture.py
+++ pgn2fixture/pgn2fixture.py
@@ -1,42 +1,93 @@
-import utils
+from . import utils
+from typing import Dict, List
 
 
 class Game:
+    """ 
+    This is a class for PGN tag extraction and handling. 
+      
+    Attributes: 
+        raw_game_data (str): The PGN game string.
+    """
 
     def __init__(self, raw_game_data) -> None:
+        """
+        The init method creates the instance variable tags.
+        The variable tags is a dictionary containing all PGN tags/data.
+        """
         self.tags = utils.extract_tag_roster(utils.clean(raw_game_data))
 
-    def __str__(self):
+    def __str__(self) -> str:
+        return f'{self.white} - {self.black} | {self.event}'
+
+    def __repr__(self) -> str:
         return f'{self.white} - {self.black} | {self.event}'
 
     @property
-    def event(self):
+    def event(self) -> str:
+        """
+        The event property returns the event PGN tag
+        """
         return self.tags['event']
 
     @property
-    def site(self):
+    def site(self) -> str:
+        """
+        The site property returns the site PGN tag
+        """
         return self.tags['site']
 
     @property
-    def date(self):
+    def date(self) -> str:
+        """
+        The date property returns the date PGN tag
+        """
         return self.tags['date']
 
     @property
-    def round(self):
+    def round(self) -> int:
+        """
+        The round property returns the round PGN tag
+        """
         return int(self.tags['round'])
 
     @property
-    def result(self):
+    def result(self) -> str:
+        """
+        The result property returns the result PGN tag
+        """
         return self.tags['result']
 
     @property
-    def white(self):
+    def white(self) -> str:
+        """
+        The white property returns the white PGN tag
+        """
         return self.tags['white']
 
     @property
-    def black(self):
+    def black(self) -> str:
+        """
+        The black property returns the black PGN tag
+        """
         return self.tags['black']
 
     @property
-    def moves(self):
+    def moves(self) -> str:
+        """
+        The moves property returns the game moves including the result
+        """
         return self.tags['moves']
+
+    def to_fixture(self, model, pk) -> Dict:
+        """
+        The to_fixture method returns the entire PGN
+        data as a dictionary ready to dump into a JSON file
+        to create Django fixtures
+        """
+        fixture = {
+            "model": model,
+            "pk": pk,
+            "fields": self.tags
+        }
+        return fixture
--- pgn2fixture/utils.py
+++ pgn2fixture/utils.py
@@ -3,12 +3,20 @@ from typing import Dict, List
 
 
 def clean(raw_pgn: str) -> List:
+    """
+    The clean method cleans the raw PGN data
+    and returns each tag as an element in a list
+    """
     str_tags = re.split(r'\]', raw_pgn, re.DOTALL)
     def cleaner(x): return x.replace('\n', '').replace('[', '')
     return list(map(cleaner, str_tags))
 
 
 def extract_tag_roster(game: List) -> Dict:
+    """
+    The extract_tag_roster transforms the tags list into a 
+    dictionary with the tag name as keys and the tag values as values
+    """
     tags = {}
     for item in game[:-1]:
         key = item.split('"')[0].lower().strip()
